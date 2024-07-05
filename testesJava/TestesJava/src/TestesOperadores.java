85a6bd16b4bd492c27f7b407520cbf455ff7c7e1
victorCNeves <concolato.neves@gmail.com>
2024-07-05 18:34:22 -0300

    criação de pasta para fazer testes de códigos java e criação de arquivo para teste operadores

---
+++ testesJava/TestesJava/src/TestesOperadores.java
@@ -0,0 +1,28 @@
+public class TestesOperadores {
+    public static void main(String[] args){
+        //Teste Unários
+        //classe Operadores.java
+        int numero = 5;
+
+        //Imprimindo o numero negativo
+        System.out.println(- numero);
+
+        //incrementando numero em mais 1 numero, imprime 6
+        numero ++;
+        System.out.println(numero);
+
+        //incrementando numero em mais 1 numero, imprime 7
+        System.out.println(numero ++);// ops algo de errado não está certo
+
+        System.out.println(numero);// agora sim, numero virou 7
+
+        //ordem de precedencia conta aqui
+        System.out.println(++ numero);
+
+        boolean verdadeiro = true;
+
+        System.out.println("Inverteu " + !verdadeiro);
+
+
+    }
+}
